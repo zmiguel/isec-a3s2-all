893e075397243876315538eb21c55a06e94e63ac
ZMiguel Alves <zmiguel.alves@gmail.com>
2017-04-03 18:54:55 +0100

    Manipulaçao de listas ligadas adicionado

--- Prog/TrabalhoPraticoProg/TrabalhoPraticoProg/funcoes.h
+++ Prog/TrabalhoPraticoProg/TrabalhoPraticoProg/funcoes.h
@@ -1,1 +1,27 @@
 #pragma once
+
+Grid * addGridIni(Grid *gamegrid, char cont) {
+	Grid *novo, *aux;
+	aux = gamegrid;
+	novo = /*(Grid *) */malloc(sizeof(Grid));
+	if (novo == NULL) exit(0);
+	novo->cont = cont;
+	novo->prox = aux->prox;
+	aux->prox = novo;
+	return(aux);
+}
+
+Grid * addGridEnd(Grid *gamegrid, char cont, int lin, int col) {
+	Grid *aux = gamegrid;
+
+	while (aux->prox != NULL) {
+		aux = aux->prox;
+	}
+	aux->prox = malloc(sizeof(Grid));
+	aux->prox->cont = cont;
+	aux->prox->lin = lin;
+	aux->prox->col = col;
+	aux->prox->prox = NULL;
+
+	return gamegrid;
+}
