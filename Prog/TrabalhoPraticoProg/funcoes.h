88a4b173878c6b7d61b209d3be0caaf2555579c3
ZMiguel Alves <zmiguel.alves@gmail.com>
2017-04-08 03:26:02 +0100

    gestao de areas a funcionar adicionada a possibilidade de criar areas novas, preciso de fazer melhores verificações aos dados intruduzidos.

--- Prog/TrabalhoPraticoProg/funcoes.h
+++ Prog/TrabalhoPraticoProg/funcoes.h
@@ -39,7 +39,7 @@ Areas * readAreas(Areas *tAreas, int *nr_areas){
 	Areas *temp = tAreas;
 	FILE *f;
 	printf("A abrir ficheiro de Areas...\n");
-	f = fopen("areas.txt", "rt");
+	f = fopen("areas.txt", "r");
 	if(f==NULL){
 		printf("Erro a abrir ficheiro de areas!\n");
 		exit(0);
@@ -50,7 +50,7 @@ Areas * readAreas(Areas *tAreas, int *nr_areas){
 		if(tNR == 1){
 			fscanf(f,"\t%d",&t1);
 			//printf("%d\t%d\t%d\t%d\t%d\n", tID, tType, tCap, tNR, t1);
-			temp = addAreaEnd(temp, tID, tType, tCap, tNR, t1, -1, -1);
+			temp = addAreaEnd(temp, tID, tType, tCap, tNR, t1, -1 , -1);
 		}else if(tNR == 2){
 			fscanf(f,"\t%d\t%d",&t1, &t2);
 			//printf("%d\t%d\t%d\t%d\t%d\t%d\n", tID, tType, tCap, tNR, t1, t2);
@@ -74,8 +74,51 @@ Areas * readAreas(Areas *tAreas, int *nr_areas){
 void dispArea(Areas *tArea){
 	Areas *temp = tArea;
 
-	while(!temp->prox == NULL){
+	while(temp->prox != NULL){
 		temp = temp->prox;
-		printf("id:%d\ttipo:%d\tcap:%d\tfront1:%d\tfront2:%d\tfront3:%d\n", temp->id, temp->tipo, temp->cap, temp->front1.id, temp->front2.id, temp->front3.id);
+		if(temp->nr_front == 1){
+			printf("id:%d\ttipo:%d\tcap:%d\tfront1:%d\n", temp->id, temp->tipo, temp->cap, temp->front1.id);
+		}else if(temp->nr_front == 2){
+			printf("id:%d\ttipo:%d\tcap:%d\tfront1:%d\tfront2:%d\n", temp->id, temp->tipo, temp->cap, temp->front1.id, temp->front2.id);
+		}else if(temp->nr_front == 3){
+			printf("id:%d\ttipo:%d\tcap:%d\tfront1:%d\tfront2:%d\tfront3:%d\n", temp->id, temp->tipo, temp->cap, temp->front1.id, temp->front2.id, temp->front3.id);
+		}
 	}
+}
+
+void saveAreas(Areas *tAreas){
+	int tID, tType, tCap, tNR, t1, t2, t3, i=0;
+	Areas *temp = tAreas;
+	FILE *f;
+	printf("A abrir ficheiro de Areas...\n");
+	f = fopen("areas.txt", "w+");
+	if(f==NULL){
+		printf("Erro a abrir ficheiro de areas!\n");
+		exit(0);
+	}
+	printf("A guardar Areas para ficheiro...\n");
+	while(temp->prox != NULL){
+		temp = temp->prox;
+		if(temp->nr_front == 1){
+			fprintf(f,"%d\t%d\t%d\t%d\t%d\n", temp->id, temp->tipo, temp->cap, temp->nr_front, temp->front1.id);
+		}else if(temp->nr_front == 2){
+			fprintf(f,"%d\t%d\t%d\t%d\t%d\t%d\n", temp->id, temp->tipo, temp->cap, temp->nr_front, temp->front1.id, temp->front2.id);
+		}else if(temp->nr_front == 3){
+			fprintf(f,"%d\t%d\t%d\t%d\t%d\t%d\t%d\n", temp->id, temp->tipo, temp->cap, temp->nr_front, temp->front1.id, temp->front2.id, temp->front3.id);
+		}
+
+	}
+	fclose(f);
+	printf("Areas guardadas!!\n");
+}
+
+int nrFront(Areas *tArea, int id){
+	Areas *temp = tArea;
+
+	do{
+		temp = temp->prox;
+		if(temp == NULL) return -1;
+	}while(temp->id != id);
+
+	return temp->nr_front;
 }
--- Prog/TrabalhoPraticoProg/main.c
+++ Prog/TrabalhoPraticoProg/main.c
@@ -5,9 +5,10 @@
 
 #include "estruturas.h"
 #include "funcoes.h"
+#include "util.h"
 
 void main(void) {
-	int menuopt, nr_areas;
+	int menuopt, nr_areas, tType=-1, tCap, tNR=0, t1=-1, t2=-2, t3=-3;
 
 	Areas *zAreas = NULL;
 	Animais *zAnimais = NULL;
@@ -21,12 +22,83 @@ void main(void) {
 	zAreas = readAreas(zAreas, &nr_areas);
 
 	printf("READY! %d\n", nr_areas);
-	dispArea(zAreas);
+	//dispArea(zAreas);
+	printf("%d\t%d\t%d\n", nrFront(zAreas,1), nrFront(zAreas,2), nrFront(zAreas,3));
 
-	//menuopt = menu();
+	for(;;){
+		menuopt = menu();
+		if(menuopt == 0) break;
+		if(menuopt == 11){
+			nr_areas++;
+			while(tType != 0 && tType != 1){
+				fflush(stdin);
+				printf("Tipo de area (0 = jaula, 1 = espaco vedado): ");
+				scanf("%d", &tType);
+			}
+			fflush(stdin);
+			printf("Capacidade maxima: ");
+			scanf("%d", &tCap);
+			while(tNR != 1 && tNR != 2 && tNR != 3){
+				fflush(stdin);
+				printf("Numero de Fronteiras (1/2/3): ");
+				scanf("%d", &tNR);
+			}
+			if(tNR == 1){
+				while(t1 >= nr_areas || t1 < 1){
+					printf("ID fronteira 1: ");
+					scanf("%d", &t1);
+				}
+				zAreas = addAreaEnd(zAreas, nr_areas, tType, tCap, tNR, t1, -1, -1);
+			}else if(tNR == 2){
+				while(t1 >= nr_areas || t1 < 1){
+					printf("ID fronteira 1: ");
+					scanf("%d", &t1);
+					while(t2 >= nr_areas || t2 < 1 || t2 == t1){
+						printf("ID fronteira 2: ");
+						scanf("%d", &t2);
+					}
+				}
+				zAreas = addAreaEnd(zAreas, nr_areas, tType, tCap, tNR, t1, t2, -1);
+			}else if(tNR == 3){
+				while(t1 >= nr_areas || t1 < 1){
+					printf("ID fronteira 1: ");
+					scanf("%d", &t1);
+					while(t2 >= nr_areas || t2 < 1 || t2 == t1){
+						printf("ID fronteira 2: ");
+						scanf("%d", &t2);
+						while(t3 >= nr_areas || t3 < 1 || t3 == t1 && t3 == t2){
+							printf("ID fronteira 2: ");
+							scanf("%d", &t3);
+						}
+					}
+				}
+				zAreas = addAreaEnd(zAreas, nr_areas, tType, tCap, tNR, t1, t2, t3);
+			}
+			printf("Area Adicionada!!\n");
+			dispArea(zAreas);
+		}
+	}
 
+	saveAreas(zAreas);
 }
 
 int menu(void){
+	int res;
 
+	printf("\nMenu:\n\t1 - Gerir Areas\n\t0 - Sair\n");
+	printf("Escolha: ");
+	fflush(stdin);
+	scanf("%d", &res);
+
+	if(res == 0){
+		return 0;
+	}else if(res == 1){
+		printf("\n\t1 - Adicionar Area\n\t2 - Remover Area\n");
+		printf("Escolha: ");
+		fflush(stdin);
+		scanf("%d", &res);
+		if(res == 1) return 11;
+		if(res == 2) return 12;
+
+	}
 }
